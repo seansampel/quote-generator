14b96186ebc731e4c5c9b8a30d33fbf35e209fe0
Sean <53915643+sampelmedia@users.noreply.github.com>
2021-02-15 11:40:52 +0000

    delete all react items

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          <h1>Quote generator</h1>
+        </p>
+ 
+      </header>
+    </div>
+  );
+}
+
+export default App;
