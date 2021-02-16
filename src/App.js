cd1dfba2028c5cb0bbf1c680287b91dd082b87c4
Sean <53915643+sampelmedia@users.noreply.github.com>
2021-02-16 09:10:24 +0000

    change of format in app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
+import axios from "axios";
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class Quote extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      Quote: '',
+    }
+  }
+
+  componentDidMount() {
+    this.newQuote();
+  }
+
+  newQuote () {
+    axios.get('https://movie-database-imdb-alternative.p.rapidapi.com/')
+    .then(res => {
+      // let data = res.data.quotes
+      // let quoteNum = Math.floor(Math.random() * data.length)
+      // let Quote = data[quoteNum]
+
+    this.state({
+      quote: Quote['quote'],
+    })
+  })
+}
+
+getNewQuote = () => {
+  this.Quote()
+}
+  
+render() {
+  const { Quote } = this.state
+
+    return (
+    <div className="Quote">
+      <header className="Generator">
 
         <p>
           <h1>Quote generator</h1>
-          <form>
-          <input
-            className="form button"
-            placeholder="..."
-            />
-          </form>
-          <button className="seachButton">Click</button>
+  
+          <button className="seachButton"
+          onClick={this.state.quote}>Click</button>
         </p>
  
       </header>
     </div>
-  );
-}
+    )
+  };
+};
 
-export default App;
+export default Quote;
