859c5e49a5b5ef01a589afa51b16a6c50dc8902c
Sean <53915643+sampelmedia@users.noreply.github.com>
2021-04-08 10:33:43 +0100

    add more styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Quotes from './components/Quotes';
+import Keano from './Keano.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header id="Keane">
+      <header>
         <h1>Roy Keane Quote Generator</h1>
+        <h5>Roy Keane is not a man to mince his words as you're about to find out!</h5>
+        <img alt='Keano' 
+        className='Keano'
+        src={Keano}/>
       </header>
       <Quotes />
     </div>
